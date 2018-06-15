6313d8179da327367e0af3ed6014c7b751ae4961
hzhounait <31709639+hzhounait@users.noreply.github.com>
2018-06-14 20:06:54 -0600

    Initial Setup

---
+++ LTWebsite/LTWebsite/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace LTWebsite.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LTWebsite/LTWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LTWebsite.Startup))]
+namespace LTWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
